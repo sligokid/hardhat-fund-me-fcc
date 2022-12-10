6dcf6543ee19c56d8b41207d968b4f3a1c480857
Kieran Mc Gowan <kieranmcgowan21@homtail.com>
2022-12-10 13:24:50 +0000

    adding fund and withdraw scripts locally

---
+++ scripts/withdraw.js
@@ -0,0 +1,20 @@
+const { getNamedAccounts, ethers } = require("hardhat")
+
+async function main() {
+    const { deployer } = await getNamedAccounts()
+    const fundMe = await ethers.getContract("FundMe")
+
+    console.log("Withdrawing... ")
+
+    const transactionResponse = await fundMe.withdraw()
+    await transactionResponse.wait(1)
+
+    console.log("Done..")
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
